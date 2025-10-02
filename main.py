9c8e99d225226c3870991e35bef19d88196791f5
chewbacca1991 <mounafbanbo@hotmail.com>
2025-10-02 19:23:56 +0330

    feat: initial commit for main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+class RestaurantDataAnalyzer:
+    def __init__(self, data_file):
+        self.data = pd.read_csv(data_file)
+
+    def analyze_data(self):
+        # Phân tích cơ bản dữ liệu
+        print(self.data.describe())
+
+    def visualize_data(self):
+        # Trực quan hóa dữ liệu
+        plt.figure(figsize=(10, 5))
+        plt.bar(self.data['Restaurant_Name'], self.data['Rating'])
+        plt.title('Đánh Giá Nhà Hàng')
+        plt.xlabel('Tên Nhà Hàng')
+        plt.ylabel('Đánh Giá')
+        plt.xticks(rotation=90)
+        plt.tight_layout()
+        plt.show()
+
+if __name__ == '__main__':
+    analyzer = RestaurantDataAnalyzer('restaurant_data.csv')
+    analyzer.analyze_data()
+    analyzer.visualize_data()
